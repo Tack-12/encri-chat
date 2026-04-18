f68a056ef78da65f1a140f2adc444b5d2b3cd9d2
Tack-12 <pranayauni1@gmail.com>
2026-04-18 12:22:09 -0500

    Merged master to main

---
+++ backend/controller/messageController.ts
@@ -0,0 +1,8 @@
+import type { Request, Response } from "express";
+
+
+const sendMessagePost = (req: Request, res: Response) => {
+        const { message, sendBy, sentTo } = req.body;
+
+
+}
---
+++ backend/routes/indexRouter.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import type { Request, Response } from "express";
+
+//Declaration:
+const indexRouter = Router();
+
+indexRouter.get("/",)
+
+
+//Exporting the routes to be used in main folder:
+export { indexRouter };
