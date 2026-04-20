4286cb4a85ec6be7ea2f0071d8b7e2dc0d718c03
Pranaya Pudasaini <pranayauni1@gmail.com>
2026-04-19 23:57:58 -0500

    Minor mistake, forgot the add controller to router

--- backend/routes/indexRouter.ts
+++ backend/routes/indexRouter.ts
@@ -4,11 +4,12 @@ import passport from "passport";
 import jwt from "jsonwebtoken";
 import "dotenv/config";
 import { auth_middleware } from "./auth";
+import { login } from "../controller/indexController";
 
 //Declaration:
 const indexRouter = Router();
 
-indexRouter.get("/login", auth_middleware,);
+indexRouter.get("/login", auth_middleware, login);
 
 
 //Exporting the routes to be used in main folder:
